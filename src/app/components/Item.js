ba92e8ea7152f56731c1bf9ec1cbe026a39cb5fd
unknown <safaaballagh97@gmail.com>
2021-11-13 15:39:13 +0100

    Style title text and services

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -5,17 +5,21 @@ import '../css/item.css';
 
 const Item = ({ item }) => {
   const page = `/details/${item.id}`;
+  const services = item.services.map((service) => (<li key={service}>{ service }</li>));
   return (
     <a className="w-50 text-decoration-none link-dark text-center text-dark" href={page}>
       <Card className="item-card border-0">
         <img src={item.img} className="card-img-top item-img" alt="..." />
         <div className="d-flex justify-content-between align-items-center px-3 pt-2">
-          <Card.Title>{item.name}</Card.Title>
+          <Card.Title className="item-name">{item.name}</Card.Title>
         </div>
         <Card.Body>
-          <Card.Text>
+          <Card.Text className="item-description">
             {item.description}
           </Card.Text>
+          <ul className="item-services d-flex">
+            {services}
+          </ul>
         </Card.Body>
       </Card>
     </a>
