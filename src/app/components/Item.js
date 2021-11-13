00a1da5f1b3e983c50febb28b7d50b0dc4334bc9
unknown <safaaballagh97@gmail.com>
2021-11-13 14:30:21 +0100

    Style item image and card

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { string } from 'prop-types';
 import { Card } from 'react-bootstrap';
+import '../css/item.css';
 
 const Item = ({ item }) => {
   const page = `/details/${item.id}`;
   return (
     <a className="w-50 text-decoration-none link-dark text-center text-dark" href={page}>
-      <Card className="w-50">
-        <img src={item.img} className="card-img-top" alt="..." />
+      <Card className="item-card border-0">
+        <img src={item.img} className="card-img-top item-img" alt="..." />
         <div className="d-flex justify-content-between align-items-center px-3 pt-2">
           <Card.Title>{item.name}</Card.Title>
         </div>
@@ -27,17 +28,17 @@ Item.propTypes = {
     name: PropTypes.string,
     img: PropTypes.string,
     description: PropTypes.string,
-    services: PropTypes.string,
+    services: PropTypes.arrayOf(string),
   }),
 };
 
 Item.defaultProps = {
   item: {
-    id: 'name',
-    name: 'Lola',
+    id: 'office1',
+    name: 'office1',
     img: 'https://user-images.githubusercontent.com/43698511/141481667-1e961fd1-d3ca-43a5-86ba-8b3d48be7c11.png',
-    description: 'Test ',
-    services: '',
+    description: 'Nice looking office',
+    services: ['wi-fi', 'ac'],
   },
 };
 
