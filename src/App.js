390f6d31dcb1d5f94d168b825710b60ac2167019
blakbox23 <boudii30@gmail.com>
2021-11-16 13:28:13 +0300

    Set up reservaion component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Carr from './app/components/Carr'
+// import Carr from './app/components/Carr';
+// import './app/css/carr.css';
+import Reservations from './app/components/Reservations';
+import './app/css/reservations.css';
 import './app/css/carr.css';
 
 function App() {
   return (
-  
-    <Carr />
+  <>
+    {/* <Carr /> */}
+
+    <Reservations />
+
+ </>
  
   );
 }
---
+++ src/app/components/Reservations.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+function Reservations() {
+  
+    return (
+      <div>
+          <h1 className="res-header">My Reservations</h1>
+
+
+
+  
+      </div>
+    );
+  }
+  
+  export default Reservations;
