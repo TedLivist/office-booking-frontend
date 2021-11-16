97b4843e1d05e64e948fcd49e54571b929af0679
unknown <safaaballagh97@gmail.com>
2021-11-16 10:48:52 +0100

    Fix linters

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/prefer-default-export */
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
