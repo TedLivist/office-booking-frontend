4c800d2a0cea3bc81ca368d00c11478e13931139
unknown <safaaballagh97@gmail.com>
2021-11-16 11:00:21 +0100

    Fix errors

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-use-before-define */
+/* eslint-disable */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
