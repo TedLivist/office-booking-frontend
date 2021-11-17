c8c27fc89ea06c70e143600ff34083e687eb6822
TedLivist <livistteddy@gmail.com>
2021-11-17 21:27:15 +0100

    Add error handling and validation to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,24 +2,36 @@ import React, { useState } from 'react';
 
 const Login = () => {
   const [fieldsInput, setFieldInput] = useState({username: '', password: ''})
+  const [error, setError] = useState('')
 
   const handleChange = (e) => {
-    setFieldInput({...fieldsInput, [e.target.name]: e.target.value})
+    setFieldInput({...fieldsInput, [e.target.name]: e.target.value.toLowerCase()})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (fieldsInput.username.trim() && fieldsInput.password.trim()) {
+      // const {username, password} = fieldsInput
+      setFieldInput({username: '', password: ''})
+    } else {
+      setError('Empty fields are not allowed')
+    }
   }
 
   return (
     <div className="row justify-content-center">
       <h1 className="mt-3 text-center">Tada!!</h1>
       <div className="col-6">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="username" className="form-label">Email address</label>
-            <input type="text" name="username" onChange={handleChange} value={fieldsInput.username} required className="form-control" id="username" aria-describedby="username" />
-            <div id="username" className="form-text">We'll never share your email with anyone else.</div>
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" name="username" onChange={handleChange} value={fieldsInput.username} placeholder="Enter username" required className="form-control" id="username" aria-describedby="username" />
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" name="password" onChange={handleChange} value={fieldsInput.password} required className="form-control" id="password" />
+            <input type="password" name="password" onChange={handleChange} value={fieldsInput.password} placeholder="Enter password" required className="form-control" id="password" />
+            {error.length > 2 ? <div id="errorHelp" className="form-text text-danger">{error}</div> : '' }
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
