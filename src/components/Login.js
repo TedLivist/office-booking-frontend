3421109be089c2b02aead1fc9c734865a9bde0b9
TedLivist <livistteddy@gmail.com>
2021-11-17 20:00:26 +0100

    Set up handleChange function for input fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [fieldsInput, setFieldInput] = useState({username: '', password: ''})
+
+  const handleChange = (e) => {
+    setFieldInput({...fieldsInput, [e.target.name]: e.target.value})
+  }
+
   return (
     <div className="row justify-content-center">
       <h1 className="mt-3 text-center">Tada!!</h1>
@@ -8,12 +14,12 @@ const Login = () => {
         <form>
           <div className="mb-3">
             <label htmlFor="username" className="form-label">Email address</label>
-            <input type="text" className="form-control" id="username" aria-describedby="username" />
+            <input type="text" name="username" onChange={handleChange} value={fieldsInput.username} required className="form-control" id="username" aria-describedby="username" />
             <div id="username" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password" />
+            <input type="password" name="password" onChange={handleChange} value={fieldsInput.password} required className="form-control" id="password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
