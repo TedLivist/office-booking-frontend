e1cb1f8912c0e137900a2f26e0b054ba8bf4594c
Lameck1 <lameck72@gmail.com>
2021-11-18 14:39:17 +0300

    Refactor App componenet

--- src/App.js
+++ src/App.js
@@ -1,30 +1,96 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getAllItems } from './redux/items/items';
 import Reserve from './components/Rerserve';
 import DeleteListItems from './components/DeleteListItems';
-import Item from './components/Item';
 import Login from './components/Login';
+import Details from './components/details/Details';
+// import Home from './components/Home';
+import Reservations from './components/Reservations';
+
+const routes = [
+  {
+    path: '/',
+    name: 'DeleteListItems',
+    component: <DeleteListItems />,
+  },
+  {
+    path: '/:username/reservations',
+    name: 'Reservations',
+    component: <Reservations />,
+  },
+  {
+    path: '/items/:id',
+    name: 'Items',
+    component: <Details />,
+  },
+  {
+    path: '/:username/reservation',
+    name: 'Reserve',
+    component: <Reserve />,
+  },
+  {
+    path: '/:username/reservation/item/:id',
+    name: 'Reserve',
+    component: <Reserve />,
+  },
+  {
+    path: '/deleteList',
+    name: 'DeleteListItems',
+    component: <DeleteListItems />,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: <Login />,
+  },
+];
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllItems());
   }, [dispatch]);
+  const username = 'Lameck';
   return (
-    <BrowserRouter>
+    <Router>
+      <nav>
+        <ul className="nav-links">
+          <NavLink
+            style={({ isActive }) => ({
+              display: 'block',
+              margin: '1rem 0',
+              color: isActive ? 'red' : '',
+            })}
+            to="/"
+          >
+            DeleteListItems
+          </NavLink>
+          <NavLink
+            style={({ isActive }) => ({
+              display: 'block',
+              margin: '1rem 0',
+              color: isActive ? 'red' : '',
+            })}
+            to={`/${username}/reservations`}
+            key={username}
+          >
+            Reservations
+          </NavLink>
+        </ul>
+      </nav>
       <Routes>
-        <Route exact path="/" element={<Item />} />
-        <Route path="/deleteList" element={<DeleteListItems />} />
-        <Route path="/:username/reservation" element={<Reserve />} />
-        <Route
-          path="/:username/reservation/item/:id"
-          element={<Reserve details />}
-        />
-        <Route path="/login" element={<Login />} />
+        {routes.map(({ path, component }) => (
+          <Route path={path} key={path} element={component} />
+        ))}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
