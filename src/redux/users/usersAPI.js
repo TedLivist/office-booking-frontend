3a4ba1c7aafb1b5660afe6e8526dd8c6b7ff0adf
blakbox23 <boudii30@gmail.com>
2021-11-19 12:01:46 +0300

    Set up fetch method for user registration

--- src/redux/users/usersAPI.js
+++ src/redux/users/usersAPI.js
@@ -19,6 +19,29 @@ export const login = async (username) => {
   return response;
 };
 
+export const signUp = async (username) => {
+  const request = await fetch('http://localhost:3000/api/v1/users', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({ username }),
+  });
+  const response = await request.json();
+  if (response) {
+    localStorage.setItem('user', JSON.stringify(response));
+  }
+
+  return response;
+};
+
+
+
 export const logout = () => {
   localStorage.removeItem('user');
 };
